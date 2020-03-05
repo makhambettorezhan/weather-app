c8d237e04b57a9a805c68786ddf68e29b98a99b5
makhambet <mtorezhan@gmail.com>
2020-03-05 14:36:48 +0600

    add yargs

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        address: {
+            demand: true,
+            alias: 'a',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+let encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyB23C1b5amqRnI0TV0rROZg6hXts0__87o&address=3110%20lombard%20street%20Philadelphia',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyB23C1b5amqRnI0TV0rROZg6hXts0__87o&address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-});
+    if(error) {
+        console.log('Unable to connect Google servers');
+    } else if(body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address');
+    } else if(body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);   
+    }});
